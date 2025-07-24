3e4008c5559ee2661cafcc45e8c0d663e336c12b
Jakreewut <nongnack2504@gmail.com>
2025-07-24 21:09:45 +0700

    Lab3

--- lab/lab3.cpp
+++ lab/lab3.cpp
@@ -2,10 +2,9 @@
 #include <math.h>
 
 int isPrime(int num) {
-    if(num<2) 
- return 0;
+    if(num<2) return 0;
     for (int i = 2; i <= sqrt(num); i++) {
-      if (num % i == 0)return 0;
+      if (num % i == 0) return 0;
     }
     return 1;
 }//end isprime
@@ -16,29 +15,27 @@ int main() {
       scanf("%d",&n);
 
     int arr[n]; 
-      for(int i=0;i<n;i++) {
+      for(int i = 0; i < n; i++ ) {
           printf("Enter value[%d] : ",i);
-         scanf("%d",&arr[i]);
-        }//end for
+          scanf("%d",&arr[i]);
+      }//end for
 
     printf("Index:  ");
-
-      for (int i = 0;i < n;i++) {
+       for (int i = 0; i < n; i++) {
          printf("%2d ", i);
         }//end for
         
      printf("\n");
 
      printf("Array:  ");
-
-       for(int i=0;i<n;i++) {
+      for(int i = 0; i < n; i++) {
          if (isPrime(arr[i]))
-           printf("%2d ", arr[i]); 
-              else
-                printf("%2s ", "#"); 
+             printf("%2d ", arr[i]); 
+         else
+             printf("%2s ", "#"); 
         }//end for
 
      printf("\n");
 
     return 0;
- }
+ } //end
