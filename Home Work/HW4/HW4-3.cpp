e57b391084b11939428c8ab9989d9158edf1dfb2
Jakreewut <nongnack2504@gmail.com>
2025-08-05 22:51:19 +0700

    HW4-3,HW4-4

---
+++ Home Work/HW4/HW4-3.cpp
@@ -0,0 +1,27 @@
+/*
+    จงกรอกหมายเลขทั้งหมดจำนวน 3 ค่า และแสดงผลรวมดังแสดงดังผลลัพธ์ด้านล่าง
+
+    Test case:
+        2 3 1
+    Output:
+        Answer = 6
+
+    Test case:
+        4 -1 3
+    Output:
+        Answer = 6
+*/
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+
+    printf("please enter vvalue (a b c) : ");
+    scanf("%d, %d, %d", &a, &b, &c);
+
+    int d = a + b + c;
+    printf("answer = %d ", d);
+
+    return 0;
+}
